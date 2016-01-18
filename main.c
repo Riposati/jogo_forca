c01a1bde8ac7b204d44be47abdcbe6924a0a2d09
Riposati <guriposa@gmail.com>
2016-01-18 03:26:50 -0200

    Jogo da forca C

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <time.h>
-#define tamanhoOpcoesPalavras 19 /// o tamanho do array - 1 pra escolher indices
+#define tamanhoOpcoesPalavras 30 /// o tamanho do array - 1 pra escolher indices
 
 int randomizaPalavra(){
 
@@ -12,10 +12,38 @@ int randomizaPalavra(){
     return num;
 }
 
-void setaPalavras(char palavras[20][50]){
+char abreArquivoComAsPalavrasDoJogo(char palavras[tamanhoOpcoesPalavras][50]){
 
+    FILE *f = fopen("palavras.txt" , "r+");
+
+    if(f==0){
+
+        printf("ERRO nao encontrei o arquivo com as palavras!\n");
+        exit(1);
+    }
+
+    int tam,i;
+    //fscanf(f,"%d", &tam);
+
+    for(i=0;i<tamanhoOpcoesPalavras;i++){
+        fscanf(f,"%s",palavras[i]);
+    }
+
+    /*for(i=0;i<tam;i++){ // pra visualizar o que o arquivo possui
+
+        printf("%s ",palavras[i]);
+    }*/
+
+    fclose(f);
+
+    return palavras;
+}
+
+void setaPalavras(char palavras[tamanhoOpcoesPalavras][50]){
+
+    palavras = abreArquivoComAsPalavrasDoJogo(palavras);
     /** aqui seto algumas opções do joguinho */
-    strcpy(palavras[0],"morango");
+    /*strcpy(palavras[0],"morango");
     strcpy(palavras[1],"abacaxi");
     strcpy(palavras[2],"melao");
     strcpy(palavras[3],"banana");
@@ -35,7 +63,7 @@ void setaPalavras(char palavras[20][50]){
     strcpy(palavras[16],"janela");
     strcpy(palavras[17],"carro");
     strcpy(palavras[18],"malandro");
-    strcpy(palavras[19],"preguica");
+    strcpy(palavras[19],"preguica");*/
     /********************************/
 }
 
@@ -59,40 +87,57 @@ char leOpcao(){
     return opcao;
 }
 
+void mostraTracosInicioJogo(char *palavra, char *tracos){
+
+    int i;
+
+    for(i=0;i<strlen(palavra);i++){ /// mostra os traços no inicio da rodada
+        printf(" %c ",tracos[i]);
+    }
+    printf("\n");
+}
+
+void mostraPerdeu(char *palavra, int chances){
+
+    if(chances==0){ /// perdeu entra aqui
+
+            printf("\n------VOCE FOI ENFORCADO!!!------\n\n--]\n  |\n  |\n  |\n \\O/\n  |\n  /\\");
+            printf("\n");
+            printf("A PALAVRA CERTA ERA --->>>> %s",palavra);
+            printf("\n");
+        }
+}
+
 int main()
 {
-    int chances = 6,i,flag,flagResp=0;
+    int chances = 6,i,flag,flagGanhouOJogo=0;
     char opcao;
 
     do{
 
         char palavra[50],caracterInformado;
-        char palavras[20][50];
+        char palavras[tamanhoOpcoesPalavras][50];
         chances = 6;
 
         setaPalavras(palavras);
         int num = randomizaPalavra();
-        //printf("num = %d\n",num);
-        //printf("palavras[num] = %s\n",palavras[num]);
         strcpy(palavra,palavras[num]); /// copia a palavra escolhida randomicamente pra palavra que iremos jogar
 
         char tracos[50];
         inicializaTracos(tracos);
 
-        puts("********************* Jogo da forca *********************\n");
-        puts("inicio....\n");
-        for(i=0;i<strlen(palavra);i++){ /// mostra os traços no inicio da rodada
-            printf(" %c ",tracos[i]);
-        }
-        printf("\n");
+        puts("********************* Jogo da forca GUSTAVO RIPOSATI *********************\n");
+        puts("vamos la, chute uma letra qualquer e tecle enter....\n");
+
+        mostraTracosInicioJogo(palavra,tracos);
 
         while(chances > 0){ /// enquanto houverem chances
 
             flag=0; /// flag de controle pra saber se o user acertou o chute o não
-            flagResp = 0; /// flag para saber se o jogador venceu o jogo e para a execução caso o tenha feito
+            flagGanhouOJogo = 0; /// flag para saber se o jogador venceu o jogo e para a execução caso o tenha feito
             scanf(" "); /// ler lixo do teclado
 
-            printf("\ninforme um caracter e tecle enter\n");
+            printf("\ninforme uma letra qualquer e tecle enter\n");
             scanf("%c",&caracterInformado);
             caracterInformado = tolower(caracterInformado); /// deixa minuscula caso seja informado valor maiusculo
 
@@ -111,12 +156,12 @@ int main()
 
                 if(tracos[i]!=palavra[i]){
 
-                    flagResp = 1;
+                    flagGanhouOJogo = 1;
                 }
             }
             printf("\n");
 
-            if(flagResp==0){
+            if(flagGanhouOJogo==0){
                 printf("VOCE GANHOU! PARABENS!\n");
                 printf(" \\O/\n  | \n  /\\");
                 break;
@@ -129,14 +174,7 @@ int main()
             }
         }
 
-        if(chances==0){ /// perdeu entra aqui
-
-            printf("\n------VOCE FOI ENFORCADO!!!------\n\n--]\n  |\n  |\n  |\n \\O/\n  |\n  /\\");
-            printf("\n");
-            printf("A PALAVRA CERTA ERA --->>>> %s",palavra);
-            printf("\n");
-            opcao = leOpcao();
-        }
+        mostraPerdeu(palavra,chances);
 
         printf("\n");
         opcao = leOpcao();
